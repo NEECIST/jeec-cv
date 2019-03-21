3529b7f97edbc01ce8c53311a1e7f3f789104cff
JosephTheCoder <menesesjmmc@gmail.com>
2019-03-21 15:37:13 +0000

    added cv_count to manage

--- backend/manage.py
+++ backend/manage.py
@@ -2,8 +2,13 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from appserver import app
 from app.database import db
-import os
+import os, os.path
 
+def count_files():
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    directory =  os.path.join(current_path, 'app', 'storage')
+    print(len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))]))
+    exit()
 
 manager = Manager(app)
 
@@ -13,6 +18,8 @@ migrations_dir = os.path.join(current_path, 'app', 'database', 'migrations')
 migrate = Migrate(app, db, directory=migrations_dir)
 
 manager.add_command('db', MigrateCommand)
+
+manager.add_command('cv_count', count_files())
 
 if __name__ == '__main__':
     manager.run()
