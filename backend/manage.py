eab157c8aa39caa9ead04dcddb301e468ed704bd
JosephTheCoder <menesesjmmc@gmail.com>
2019-04-01 17:59:15 +0100

    added command to get submited istids

--- backend/manage.py
+++ backend/manage.py
@@ -10,6 +10,22 @@ def count_files():
     print('Number of files in storage: ' + str(len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])))
     exit()
 
+def get_submissions_info():
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    directory =  os.path.join(current_path, 'app', 'storage')  
+
+    output_file = open('submissions_info', 'w+')
+        
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            start = 3
+            end = len(filename) - 4
+            output_file.write(filename[start:end] + '\n')
+        
+    output_file.close()
+    print("Info file was created sucessfully!")
+    exit()
+
 manager = Manager(app)
 
 current_path = os.path.dirname(os.path.realpath(__file__))
@@ -18,6 +34,8 @@ migrations_dir = os.path.join(current_path, 'app', 'database', 'migrations')
 migrate = Migrate(app, db, directory=migrations_dir)
 
 manager.add_command('db', MigrateCommand)
+manager.add_command('cv_count', count_files())
+manager.add_command('get_info_file', get_submissions_info())
 
 
 if __name__ == '__main__':
